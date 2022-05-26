b62f636acb55d80c0a684967654081c2ec4514e4
midays <midays@redhat.com>
2022-05-26 15:41:16 +0300

    made login in a different folder

--- cypress/integration/tests/general.test.ts
+++ cypress/integration/tests/general.test.ts
@@ -2,17 +2,6 @@ import { createProject, deleteProject, loginToSource } from "../../utils/oc_wrap
 import { clickByText, editTargetNamespace, fillGeneralFields, login, navigateToPage, next, searchAndSelectNamespace } from "../../utils/utils";
 import { pages } from "../views/enums.view";
 
-describe('Log In', () => {
-
-    it('Login to MTC UI', () => {
-        login();
-
-        // Assert that home page has loaded after login
-        cy.get('h1').should('contain', 'Migration Toolkit for Containers')
-    });
-});
-
-
 describe('mtc-343-target-cluster-field-editable', () =>{
     
     it('mtc-343-target-cluster-field-editable', () =>{
