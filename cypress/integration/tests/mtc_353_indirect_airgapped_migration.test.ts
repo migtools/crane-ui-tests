bb9c3b10f7fdbe13adb3f9e44c74f5f93c94aae3
midays <midays@redhat.com>
2022-06-07 20:12:12 +0300

    - fixes here & there, I don't really remember what I did, I coded a lot of things :-/ LOL!

--- cypress/integration/tests/mtc_353_indirect_airgapped_migration.test.ts
+++ cypress/integration/tests/mtc_353_indirect_airgapped_migration.test.ts
@@ -36,13 +36,17 @@ describe('mtc-353-indirect-air-gapped-migration', () => {
 
             ($el.stdout == '') ? proxy_str = '' : proxy_str = $el.stdout.split(": ")[1].trim()
 
-            cy.exec(`"${craneConfigurationScript}" setup_crane ${sourceCluster} ${targetCluster} ${proxy_str}`, { timeout: 600000 })
+            cy.exec(`"${craneConfigurationScript}" setup_crane "${sourceCluster}" "${targetCluster}" "${proxy_str}"`, { timeout: 600000 })
+                .then((result) => {
+                    console.log(result.stderr)
+                    debugger
+                })
                 .its('stdout')
                 .should('contain', 'SSL Certificate generation complete');
         });
 
-        cy.exec(`"${configurationScript}" setup_source_cluster ${planData.namespaceList} ${sourceCluster}`, { timeout: 200000 });
-        cy.exec(`"${configurationScript}" setup_target_cluster ${planData.namespaceList} ${targetCluster}`, { timeout: 200000 });
+        cy.exec(`"${configurationScript}" setup_source_cluster ${planData.namespaceList} "${sourceCluster}"`, { timeout: 200000 });
+        cy.exec(`"${configurationScript}" setup_target_cluster ${planData.namespaceList} "${targetCluster}"`, { timeout: 200000 });
     });
 
     // login
@@ -55,7 +59,7 @@ describe('mtc-353-indirect-air-gapped-migration', () => {
     it('Add new cluster', () => {
         run_command_oc('source', "sa get-token -n openshift-migration migration-controller").then(($el) => {
             const url = 'https://proxied-cluster.openvpn-tunnel-namespace.svc.cluster.local:8443'
-            const registry_path = null
+            const registry_path = 'proxied-cluster.openvpn-tunnel-namespace.svc.cluster.local:5000'
             const clusterData = {
                 name: 'vpn-tunnel-source',
                 url: url,
@@ -79,8 +83,8 @@ describe('mtc-353-indirect-air-gapped-migration', () => {
 
     // validate & clean
     after('Validat Migration & Clean up resources', () => {
-        cy.exec(`"${configurationScript}" post_migration_verification_on_target ${planData.namespaceList} ${targetCluster}`, { timeout: 100000 });
-        cy.exec(`"${configurationScript}" cleanup_source_cluster ${planData.namespaceList} ${sourceCluster}`, { timeout: 100000 });
-        cy.exec(`"${craneConfigurationScript}" clean_crane ${sourceCluster} ${targetCluster}`);
+        cy.exec(`"${configurationScript}" post_migration_verification_on_target ${planData.namespaceList} "${targetCluster}"`, { timeout: 100000 });
+        cy.exec(`"${configurationScript}" cleanup_source_cluster ${planData.namespaceList} "${sourceCluster}"`, { timeout: 100000 });
+//         cy.exec(`"${craneConfigurationScript}" clean_crane "${sourceCluster}" "${targetCluster}"`);
     });
 });
--- cypress/utils/crane_configuration_script.sh
+++ cypress/utils/crane_configuration_script.sh
@@ -1,14 +1,28 @@
 SRC_CLUSTER=$2
 TGT_CLUSTER=$3
 PROXY_STR=$4
+NAMESPACE="openvpn-tunnel-namespace"
 
 setup_crane() {
 
-  oc login "${SRC_CLUSTER}" --insecure-skip-tls-verify
+  oc login ${SRC_CLUSTER} --insecure-skip-tls-verify
 
-  if (oc get project openvpn-tunnel-namespace 2>/dev/null); then
-    oc delete project openvpn-tunnel-namespace
-    sleep 30
+  if (oc get project ${NAMESPACE} 2>/dev/null); then
+    oc delete project ${NAMESPACE}
+    while [[ $(oc get project ${NAMESPACE} 2>/dev/null) ]]; do
+      sleep 5
+    done
+  fi
+
+  SOURCE_CONTEXT=$(oc config current-context)
+
+  oc login ${TGT_CLUSTER} --insecure-skip-tls-verify
+
+  if (oc get project ${NAMESPACE} 2>/dev/null); then
+    oc delete project ${NAMESPACE}
+    while [[ $(oc get project ${NAMESPACE} 2>/dev/null) ]]; do
+      sleep 5
+    done
   fi
 
   # mkdir crane_temp
@@ -17,19 +31,10 @@ setup_crane() {
 
   chmod +x ./crane
 
-  SOURCE_CONTEXT=$(oc config current-context)
-
-  oc login "${TGT_CLUSTER}" --insecure-skip-tls-verify
-
-  if (oc get project openvpn-tunnel-namespace 2>/dev/null); then
-    oc delete project openvpn-tunnel-namespace
-    sleep 30
-  fi
-
   DESTINATION_CONTEXT=$(oc config current-context)
 
   if [[ $PROXY_STR == "" ]]; then
-    ./crane tunnel-api --namespace openvpn-tunnel-namespace --destination-context "${DESTINATION_CONTEXT}" --source-context "${SOURCE_CONTEXT}"
+    ./crane tunnel-api --namespace ${NAMESPACE} --destination-context ${DESTINATION_CONTEXT} --source-context ${SOURCE_CONTEXT}
   else
 
     PROXY_USER="$(cut -d':' -f2 <<<"$PROXY_STR" | cut -d'/' -f3)"
@@ -37,30 +42,34 @@ setup_crane() {
     PROXY_HOST="$(cut -d'@' -f2 <<<"$PROXY_STR" | cut -d':' -f1)"
     PROXY_PORT="$(cut -d'@' -f2 <<<"$PROXY_STR" | cut -d':' -f2)"
 
-    ./crane tunnel-api --namespace openvpn-tunnel-namespace --destination-context "${DESTINATION_CONTEXT}" --source-context "${SOURCE_CONTEXT}" --proxy-host "${PROXY_HOST}" --proxy-port "${PROXY_PORT}" --proxy-user "${PROXY_USER}" --proxy-pass "${PROXY_PASS}"
+    ./crane tunnel-api --namespace ${NAMESPACE} --destination-context ${DESTINATION_CONTEXT} --source-context ${SOURCE_CONTEXT} --proxy-host ${PROXY_HOST} --proxy-port ${PROXY_PORT} --proxy-user ${PROXY_USER} --proxy-pass ${PROXY_PASS}
   fi
 
 }
 
 clean_crane() {
 
-  oc login "${SRC_CLUSTER}" --insecure-skip-tls-verify
+  oc login ${SRC_CLUSTER} --insecure-skip-tls-verify
 
-  if (oc get project openvpn-tunnel-namespace 2>/dev/null); then
-    oc delete project openvpn-tunnel-namespace
-    sleep 30
+  if (oc get project ${NAMESPACE} 2>/dev/null); then
+    oc delete project ${NAMESPACE}
+    while [[ $(oc get project ${NAMESPACE} 2>/dev/null) ]]; do
+      sleep 5
+    done
   fi
 
-  oc login "${TGT_CLUSTER}" --insecure-skip-tls-verify
+  oc login ${TGT_CLUSTER} --insecure-skip-tls-verify
 
-  if (oc get project openvpn-tunnel-namespace 2>/dev/null); then
-    oc delete project openvpn-tunnel-namespace
-    sleep 30
+  if (oc get project ${NAMESPACE} 2>/dev/null); then
+    oc delete project ${NAMESPACE}
+    while [[ $(oc get project ${NAMESPACE} 2>/dev/null) ]]; do
+      sleep 5
+    done
   fi
 }
 
 if [ $1 == "setup_crane" ]; then
-  setup_crane SRC_CLUSTER TGT_CLUSTER
+  setup_crane SRC_CLUSTER TGT_CLUSTER PROXY_STR
 elif [ $1 == "clean_crane" ]; then
-  clean_crane
+  clean_crane SRC_CLUSTER TGT_CLUSTER
 fi
