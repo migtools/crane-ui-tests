e35215fdb4d0cd222a7c60a5edb1afbffd04f644
midays <midays@redhat.com>
2022-08-21 16:47:43 +0300

    Sometimes the before block fails due to connectivity issues, therefore it needs to be retried, but sadly this is not available in cypress yet, so the tag was changed from 'before' to 'it'

--- cypress/integration/tests/sanity.test.ts
+++ cypress/integration/tests/sanity.test.ts
@@ -57,7 +57,7 @@ selectorTuple.forEach(($type) => {
     describe(`'${migrationType}'`, () => {
 
         // run before the all coming tests
-        before('Setting up Clusters', () => {
+        it('Setting up Clusters', () => {
             // cy.wait(10000)
             if (['Storage class conversion', 'State migration'].indexOf(`${planData.migration_type}`) > -1) {
 
